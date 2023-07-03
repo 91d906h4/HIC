6e93c22f2b207f1f6a88c1b5734698e2ba0ebbe0
91d906h4 <fairymanning@gmail.com>
2023-07-03 19:21:33 +0900

    update

--- v7/compiler.py
+++ v7/compiler.py
@@ -2,7 +2,7 @@ from hLexer import Lexer
 from hParser import Parser
 from hGenerator import Generator
 
-raw = open("./test/def_test.hy").read()
+raw = open("./test/test.hy").read()
 
 # Lexer
 tokens = Lexer(raw).getTokens()
